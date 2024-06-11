8c8722c591e2e0dcd63918c8e3993158fd8624ad
N-DucharmeBarth-NOAA <nicholas.ducharme-barth@noaa.gov>
2024-06-11 08:09:18 -1000

    update container

--- .devcontainer/setup.r
+++ .devcontainer/setup.r
@@ -1,6 +1,2 @@
-# Install R packages
-install.packages("languageserver")
-install.packages("jsonlite") 
-install.packages("renv")
 # Restore the renv packages on container build
 renv::restore(prompt=FALSE)
